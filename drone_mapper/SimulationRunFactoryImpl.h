a86fc66145d632c2ece79cb2251bd97438b2b83b
Amit Reif-Tagari <rtamit10@gmail.com>
2026-06-16 14:58:53 +0300

    14:58

--- drone_mapper/SimulationRunFactoryImpl.h
+++ drone_mapper/SimulationRunFactoryImpl.h
@@ -1,18 +1,20 @@
 #pragma once
+
 #include <drone_mapper/ISimulationRunFactory.h>
+#include <drone_mapper/SimulationRunImpl.h>
+#include <filesystem>
 #include <memory>
 
 namespace drone_mapper {
-class SimulationRunFactoryImpl final : public ISimulationRunFactory {
-public:
-    SimulationRunFactoryImpl() = default;
-    ~SimulationRunFactoryImpl() override = default;
 
-    [[nodiscard]] std::unique_ptr<ISimulationRun>
-    create(const types::SimulationConfigData& simulation,
-           const types::MissionConfigData& mission,
-           const types::DroneConfigData& drone,
-           const types::LidarConfigData& lidar,
-           const std::filesystem::path& output_path) override;
+class SimulationRunFactoryImpl : public ISimulationRunFactory {
+public:
+    std::unique_ptr<ISimulationRun> create(
+        const types::SimulationConfigData& simulation,
+        const types::MissionConfigData& mission,
+        const types::DroneConfigData& drone,
+        const types::LidarConfigData& lidar,
+        const std::filesystem::path& output_path) override;
 };
+
 } // namespace drone_mapper
